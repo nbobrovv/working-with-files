2f05129a58f3ad8a4e69e76f6f062c50661ddc92
nbobrov8 <bobrovn8@yandex.ru>
2021-11-21 23:32:28 +0300

    added tasks

--- tasks/individual1.py
+++ tasks/individual1.py
@@ -6,10 +6,11 @@
 три предложения и выводит их в обратном порядке.
 """
 
+
 if __name__ == "__main__":
     with open("individual1.txt", "r") as fileptr:
         sentences = fileptr.readlines()
         print(sentences)
         print()
-        reverse_sentences = f"\n".join(reversed(sentences))
+        reverse_sentences = " ".join(reversed(sentences))
         print(reverse_sentences)
